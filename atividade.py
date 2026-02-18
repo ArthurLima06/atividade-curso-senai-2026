6924dedccba62e47d20c5299e4935e9496bd59de
Arthur Lima <tuolima366@gmail.com>
2026-02-18 17:17:57 -0300

    first commit

---
+++ atividade.py
@@ -0,0 +1,9 @@
+nome_cliente = ["Wanderson", "Priscila", "Bianca"]
+idade_clientes = [50, 45, 25]
+renda_cliente = [3200.00, 5300.00, 1200.00]
+
+
+informacao = input(str("Qual o seu nome ?:"))
+
+def validar_credito():
+    if cliente[i]
