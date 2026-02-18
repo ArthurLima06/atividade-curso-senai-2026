c66597c303ee923f9ed3ccf8ba4a09ea1e20630a
Arthur Lima <tuolima366@gmail.com>
2026-02-18 17:42:33 -0300

    commit

--- atividade.py
+++ atividade.py
@@ -3,16 +3,16 @@ clientes = {["Wanderson", 50, 3200.00, "bom"],
             ["Bianca", 25, 1200.00, "ruim"]}
 
 def validar_credito():
-    if clientes[i,1] >= 50 and clientes[i,2] <= 1000 and clientes[i,3] == "ruim":
+    if clientes[i][1] >= 50 and clientes[i][2] <= 1000 and clientes[i][3] == "ruim":
         print ("ALTO RISCO")
-    elif clientes[i,1] >= 50 and clientes[i,2] >= 1000 and clientes[i,3] == "bom":
+    elif clientes[i][1] >= 50 and clientes[i][2] >= 1000 and clientes[i][3] == "bom":
         print ("MÉDIO RISCO")
-    elif clientes[i,1] >= 20 and clientes [i,2] >= 3000 and clientes[i,3] == "bom":
-        print ("BAICO RISCO")
-    elif clientes[i,3] == "ruim":
+    elif clientes[i][1] >= 20 and clientes [i][2] >= 3000 and clientes[i][3] == "bom":
+        print ("BAIXO RISCO")
+    elif clientes[i][3] == "ruim":
         print ("ALTO RISCO")
     else:
         print ("SEM RESPOSTA")
 
 
-   print (validar_credito())
+print (validar_credito())
