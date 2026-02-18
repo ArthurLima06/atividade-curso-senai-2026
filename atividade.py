617d784505825bde5bc2119981ffefb10890d44e
Arthur Lima <tuolima366@gmail.com>
2026-02-18 17:35:08 -0300

    commit

--- atividade.py
+++ atividade.py
@@ -1,9 +1,16 @@
-nome_cliente = ["Wanderson", "Priscila", "Bianca"]
-idade_clientes = [50, 45, 25]
-renda_cliente = [3200.00, 5300.00, 1200.00]
-
-
-informacao = input(str("Qual o seu nome ?:"))
+clientes = {["Wanderson", 50, 3200.00, "bom"],
+            ["Priscila", 45, 5300.00, "bom"],
+            ["Bianca", 25, 1200.00, "ruim"]}
 
 def validar_credito():
-    if cliente[i]
+    if clientes[i,1] >= 50 and clientes[i,2] <= 1000 and clientes[i,3] == "ruim":
+        print ("ALTO RISCO")
+    elif clientes[i,1] >= 50 and clientes[i,2] >= 1000 and clientes[i,3] == "bom":
+        print ("MÉDIO RISCO")
+    elif clientes[i,1] >= 20 and clientes [i,2] >= 3000 and clientes[i,3] == "bom":
+        print ("BAICO RISCO")
+    else:
+        print ("SEM RESPOSTA")
+
+
+   print (validar_credito()) 
